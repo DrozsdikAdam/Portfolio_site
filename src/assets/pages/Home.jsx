342769cef1be96d2efe94c32b9bdb9bb18c8fc3b
DrozsdikAdam <drozsdik17@gmail.com>
2025-06-02 20:03:08 +0200

    packages and styles

---
+++ src/assets/pages/Home.jsx
@@ -0,0 +1,11 @@
+export const Home = () => {
+  return (
+    <div className="min-h-screen ">
+      {/*Theme toggle*/}
+      {/*background effect*/}
+      {/*Navbar */}
+      {/*Main Content*/}
+      {/*Footer */}
+    </div>
+  );
+};
